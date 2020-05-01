20f5bfd47a2c5863471458a9645fec5c33d744b4
BroodMaiden <41172576+BroodMaiden@users.noreply.github.com>
2020-05-01 17:53:45 -0300

    v1.0

---
+++ build/precache-manifest.0ac4068416b6b093efff09233374a8f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a67f8526524d7c3776b462937e620050",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e901bf88cea6f4181205",
+    "url": "/static/js/2.baae7598.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.baae7598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2034c488b59860d6172",
+    "url": "/static/js/main.89ab6deb.chunk.js"
+  },
+  {
+    "revision": "6ebb3750e6873afc70de",
+    "url": "/static/js/runtime-main.d4c2ee40.js"
+  }
+]);
